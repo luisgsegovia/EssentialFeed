a52349cd42cc5913c60305bd19e68b1f27f529bd
Luis Segovia <luissegovia@outlook.com>
2023-04-10 16:31:16 -0600

    Create Feed Item structure representation

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Luis Segovia on 10/04/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
