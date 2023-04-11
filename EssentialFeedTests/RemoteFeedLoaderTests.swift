e0165333b2fa537adad83ba2fafa4feb0908767f
Luis Segovia <luissegovia@outlook.com>
2023-04-10 19:39:45 -0600

    Combine spy captures params  into a single type

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -60,16 +60,17 @@ final class RemoteFeedLoaderTests: XCTestCase {
 }
 
 class HTTPClientSpy: HTTPClient {
-    var requestedURLs = [URL]()
-    var error: Error?
-    var completions = [(Error) -> Void]()
+    var requestedURLs: [URL] {
+        return messages.map { $0.url }
+    }
+
+    private var messages = [(url: URL, completion: (Error) -> Void)]()
 
     func get(from url: URL, completion: @escaping (Error) -> Void) {
-        completions.append(completion)
-        requestedURLs.append(url)
+        messages.append((url, completion))
     }
 
     func complete(with error: Error, at index: Int = .zero) {
-        completions[index](error)
+        messages[index].completion(error)
     }
 }
